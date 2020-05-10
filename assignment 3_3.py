6827e7f69215a49a8320294e942f4035accc6dd0
PraveenPints <64846420+PraveenPints@users.noreply.github.com>
2020-05-10 20:36:23 +0530

    Update assignment 3_3.py

--- assignment 3_3.py
+++ assignment 3_3.py
@@ -15,21 +15,21 @@ s = float(score)
 if 0.0 <= s <= 1.0:
     if s >= 0.9:
         result = "A"
-        print result
+        print("result")
     elif 0.8 <= s < 0.9:
         result = "B"
-        print result
+        print("result")
     elif 0.7 <= s < 0.8:
         result = "C"
-        print result
+        print("result")
     elif 0.6 <= s < 0.7:
         result = "D"
-        print result
+        print("result")
     else:
         result = "F"
-        print result
+        print("result")
 else:
-    print "Error: score out of range"
+    print("Error: score out of range")
     
 '''
 Desired Output:
